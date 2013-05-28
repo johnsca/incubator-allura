253dbae1f9b27ba9766a50018e6942edaba4d167
Cory Johns <cjohns@slashdotmedia.com>
2013-05-28 19:26:58 +0000

    [#6218] Deprecated and removed references to cached heads, branches and tags
    Signed-off-by: Cory Johns <cjohns@slashdotmedia.com>

--- ForgeUserStats/forgeuserstats/tests/test_stats.py
+++ ForgeUserStats/forgeuserstats/tests/test_stats.py
@@ -191,8 +191,7 @@ class TestGitCommit(unittest.TestCase, TestController):
         c.app.repo.name = 'testgit.git'
         self.repo = c.app.repo
         self.repo.refresh()
-        self.rev = M.repo.Commit.query.get(_id=self.repo.heads[0]['object_id'])
-        self.rev.repo = self.repo
+        self.rev = self.repo.commit()
 
     @td.with_user_project('test-admin')
     def test_commit(self):
